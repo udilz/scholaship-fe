77cffc01642c036b99d5d9147cd76225241f8bcf
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2024-09-29 10:35:05 +0700

    add checkbox

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,24 +1,42 @@
 import * as RadixCheckbox from '@radix-ui/react-checkbox';
-import { tv } from 'tailwind-variants';
+import { tv, VariantProps } from 'tailwind-variants';
+import { twMerge } from 'tailwind-merge';
 import { Check } from 'lucide-react';
 
-interface ICheckbox {
-    label: string,
+const checkBoxStyle = tv({
+  base: 'flex items-center justify-center border rounded-md gap-2',
+  variants: {
+    variant: {
+      primary: 'border-primary-500 peer-checked:bg-primary-500 peer-checked:border-primary-500',
+      secondary: ' border-secondary-50/50 text-black peer-checked:bg-secondary-500 peer-checked:border-secondary-500',
+      violet: 'border-violet-600 peer-checked:bg-violet-600 peer-checked:border-violet-600',
+    },
+    size: {
+      sm: 'w-4 h-4 p-2',
+      md: 'w-5 h-5 p-2',
+      lg: 'w-6 h-6 p-2',
+    }
+  },
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md'
+  },
+});
 
+type TCheckBox = VariantProps<typeof checkBoxStyle>;
+interface CheckboxProps extends TCheckBox, React.ComponentPropsWithRef<typeof RadixCheckbox.Root> {
+  label: string
 }
-// belum jadi
-export const Checkbox = ({label}: ICheckbox) => {
-  return (
-    <div className='flex items-center gap-1'>
-    <RadixCheckbox.Root className="size-5 flex items-center justify-center bg-red-300 p-2 border rounded-md" defaultChecked id="c1">
-      <RadixCheckbox.Indicator className="">
-        <Check size={10}/>
-      </RadixCheckbox.Indicator>
-    </RadixCheckbox.Root>
-    <label htmlFor="c1">
-      {label}
-    </label>
-  </div>
 
+export const Checkbox = (props: CheckboxProps) => {
+  return (
+    <div className="flex items-center gap-1">
+      <RadixCheckbox.Root className={twMerge(checkBoxStyle({ ...props }), props.className)} defaultChecked id={props.label}>
+        <RadixCheckbox.Indicator className="perr-checked:bg-blue-200">
+          <Check size={10} />
+        </RadixCheckbox.Indicator>
+      </RadixCheckbox.Root>
+      <label htmlFor={props.label} className='peer'>{props.label}</label>
+    </div>
   );
 };
