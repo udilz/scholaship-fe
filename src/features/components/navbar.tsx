c987a39d5d01b9df1df7ca79d6c7940b1a124d43
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2024-10-01 22:50:59 +0700

    profile in navbar

--- src/features/components/navbar.tsx
+++ src/features/components/navbar.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { User } from 'lucide-react';
 
 export const Navbar = () => {
   return (
@@ -7,9 +8,19 @@ export const Navbar = () => {
       <div className="font-medium tracking-tight">Scholarship AI.</div>
       <div className="flex items-center gap-6 font-medium">
         <div className="space-x-4">
-          <AtciveNavLink link='prompt' label='Home'/>
-          <AtciveNavLink link='bookmark' label='Bookmark'/>
+          <AtciveNavLink link="prompt" label="Home" />
+          <AtciveNavLink link="bookmark" label="Bookmark" />
         </div>
+      </div>
+      <div className="flex gap-2">
+        <section className='flex gap-2 items-center'>
+        <span>Arie </span>
+        <Link to={'/ptofile'}>
+          <Button className="rounded-full" variant="violet" iconOnly>
+            <User></User>
+          </Button>
+        </Link>
+       </section>
         <Button variant="violet">Logout</Button>
       </div>
     </header>
@@ -17,14 +28,14 @@ export const Navbar = () => {
 };
 
 interface IActiveNavLinkProps {
-    link : string,
-    label : string
+  link: string;
+  label: string;
 }
 
-const AtciveNavLink = ({link, label} : IActiveNavLinkProps) => {
-    return (
-        <NavLink to={`/${link}`} className={({ isActive }) => (isActive ? 'text-violet-500 font-semibold' : 'text-gray-900 hover:text-violet-500')}>
-            {label}
-        </NavLink>
-    )
-}
+const AtciveNavLink = ({ link, label }: IActiveNavLinkProps) => {
+  return (
+    <NavLink to={`/${link}`} className={({ isActive }) => (isActive ? 'font-semibold text-violet-500' : 'text-gray-900 hover:text-violet-500')}>
+      {label}
+    </NavLink>
+  );
+};
